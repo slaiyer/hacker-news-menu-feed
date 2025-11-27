f5d8fa437a87004f3b324b2124f3d6fc3519e5da
slaiyer <1776970+slaiyer@users.noreply.github.com>
2025-11-27 22:04:53 +0530

    reformat

--- Hacker News Menu Feed/LocalDataSource.swift
+++ Hacker News Menu Feed/LocalDataSource.swift
@@ -6,16 +6,16 @@ class LocalDataSource {
       UserDefaults.standard.set(encoded, forKey: "Posts")
     }
   }
-
+  
   public static func getPosts() -> [StoryFetchResponse] {
     var posts: [StoryFetchResponse] = []
-
+    
     if let data = UserDefaults.standard.data(forKey: "Posts") {
       if let decoded = try? JSONDecoder().decode([StoryFetchResponse].self, from: data) {
         posts = decoded
       }
     }
-
+    
     return posts
   }
   
@@ -24,34 +24,34 @@ class LocalDataSource {
       UserDefaults.standard.set(encoded, forKey: "SortKey")
     }
   }
-
+  
   public static func getSortKey() -> SortKey {
     var sortKey: SortKey = .original
-
+    
     if let data = UserDefaults.standard.data(forKey: "SortKey") {
       if let decoded = try? JSONDecoder().decode(SortKey.self, from: data) {
         sortKey = decoded
       }
     }
-
+    
     return sortKey
   }
-
+  
   public static func saveShowHeadline(value: Bool) {
     if let encoded = try? JSONEncoder().encode(value) {
       UserDefaults.standard.set(encoded, forKey: "ShowHeadline")
     }
   }
-
+  
   public static func getShowHeadline() -> Bool {
     var result: Bool = true
-
+    
     if let data = UserDefaults.standard.data(forKey: "ShowHeadline") {
       if let decoded = try? JSONDecoder().decode(Bool.self, from: data) {
         result = decoded
       }
     }
-
+    
     return result
   }
 }
